b93de6bb6c57ca9100dd036318883f7c36014d8c
Andrew Ford <andrew@andrewford.co.nz>
2021-12-06 08:58:52 +1300

    Tweaks and Changes

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "../styles/footer.module.css";
 
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerCont}>
+        <nav className={styles.footerNav}>
+          <ul>
+            <li>Find a Store</li>
+            <li>Sign up for Email</li>
+            <li>Become a Member</li>
+            <li>Feedback</li>
+          </ul>
+        </nav>
+        <div className={styles.copyright}>&copy; 2021 Shoe Shop</div>
+      </div>
+    </footer>
+  );
 };
