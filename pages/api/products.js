b451e6b18e9468d5615127de41bfe5ad63296d44
Andrew Ford <andrew@andrewford.co.nz>
2022-10-13 14:34:05 +1300

    Tidy up of project removed px removed old comment

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
