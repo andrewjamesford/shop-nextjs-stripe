0c0ca32e23f32746ab881b108d82202dc0f9e91f
Andrew Ford <andrew@andrewford.co.nz>
2022-10-15 20:22:21 +1300

    Changes to product purchase

--- components/products.js
+++ components/products.js
@@ -3,28 +3,32 @@ import Image from "next/image";
 import styles from "../styles/products.module.css";
 
 export const Products = ({ products }) => {
-  const handleAddToCart = (e) => {
-    e.preventDefault();
-  };
   return (
     <>
       {products.length ? (
         <ul className={styles.products}>
           {products.map((product) => (
             <li key={product.id}>
-              <Image
-                src={product.images[0]}
-                alt={`Image of ${product.name}`}
-                layout={"responsive"}
-                width={0}
-                height={0}
-                priority={true}
-              />
-              <h2>{product.name}</h2>
-              <p>{product.description}</p>
-              <a href="#" className={styles.link} onClick={handleAddToCart}>
-                Add To Cart
-              </a>
+              <form action="/api/checkout_sessions" method="POST">
+                <Image
+                  src={product.images[0]}
+                  alt={`Image of ${product.name}`}
+                  layout={"responsive"}
+                  width={0}
+                  height={0}
+                  priority={true}
+                />
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+                <button type="submit" role="link" className={styles.link}>
+                  Buy Now
+                </button>
+                <input
+                  type="hidden"
+                  name="priceId"
+                  value={product.default_price}
+                />
+              </form>
             </li>
           ))}
         </ul>
