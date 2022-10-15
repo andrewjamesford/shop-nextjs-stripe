4aed986994615461d0bcd9039091136cf8372926
Andrew Ford <andrew@andrewford.co.nz>
2022-10-15 20:31:29 +1300

    Removed button for buy now

--- components/products.js
+++ components/products.js
@@ -9,26 +9,16 @@ export const Products = ({ products }) => {
         <ul className={styles.products}>
           {products.map((product) => (
             <li key={product.id}>
-              <form action="/api/checkout_sessions" method="POST">
-                <Image
-                  src={product.images[0]}
-                  alt={`Image of ${product.name}`}
-                  layout={"responsive"}
-                  width={0}
-                  height={0}
-                  priority={true}
-                />
-                <h2>{product.name}</h2>
-                <p>{product.description}</p>
-                <button type="submit" role="link" className={styles.link}>
-                  Buy Now
-                </button>
-                <input
-                  type="hidden"
-                  name="priceId"
-                  value={product.default_price}
-                />
-              </form>
+              <Image
+                src={product.images[0]}
+                alt={`Image of ${product.name}`}
+                layout={"responsive"}
+                width={0}
+                height={0}
+                priority={true}
+              />
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
             </li>
           ))}
         </ul>
